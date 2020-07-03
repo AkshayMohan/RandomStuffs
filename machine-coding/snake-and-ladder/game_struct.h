f22395e1dfd3d5a0e7a7c89571bd480e0b84a149
Akshay Mohan <akshayceal@gmail.com>
2020-07-03 23:15:41 +0530

    Create game_struct.h

---
+++ machine-coding/snake-and-ladder/game_struct.h
@@ -0,0 +1,18 @@
+#ifndef SNL_GAME_STRUCT_INCLUDED
+#define SNL_GAME_STRUCT_INCLUDED
+
+#include <string>
+
+struct GAME_STRUCT {
+
+	int
+		start_point,
+		end_point;
+};
+
+struct PLAYER_INPUT_DATA {
+
+	std::string name;
+};
+
+#endif
